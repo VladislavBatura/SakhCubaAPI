8b2fbb04001d97dd9db1c53651c4392c47357e0c
Vladislav Batura <batura.vlad@gmail.com>
2022-06-02 19:23:08 +0700

    Implementing logged navbar

--- SakhCubaAPI/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ SakhCubaAPI/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { expand } from 'rxjs/operators';
+import { setTimeout } from 'timers';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   isExpanded = false;
+  isLogged = false;
+  _authService: AuthService;
+
+  constructor(authService: AuthService)
+  {
+    this._authService = authService;
+  }
+
+  ngOnInit(){
+    if (this._authService.isLoggedIn()){
+      this.isLogged = true;
+    }
+  }
 
   collapse() {
     this.isExpanded = false;
